555380902b6cf672a58de6303193cc7dc1589fe8
楊逸平 <75960978+06160193@users.noreply.github.com>
2021-03-10 19:19:27 +0800

    W3

---
+++ W3：GPIO程式設計/sr04_2.py
@@ -0,0 +1,68 @@
+
+import RPi.GPIO as GPIO
+import time
+v=34300
+
+output_pin = 12
+
+TRIGGER_PIN=16
+ECHO_PIN=18
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(TRIGGER_PIN, GPIO.OUT)
+GPIO.setup(ECHO_PIN, GPIO.IN)
+
+GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.HIGH)
+def measure() :
+   GPIO.output(TRIGGER_PIN, GPIO.HIGH)
+   time.sleep(0.00001)  #10us
+   GPIO.output(TRIGGER_PIN, GPIO.LOW)
+
+   
+   while GPIO.input(ECHO_PIN) == GPIO.LOW :
+ 
+        pulse_start=time.time()
+        
+   while GPIO.input(ECHO_PIN) == GPIO.HIGH :
+        pulse_end=time.time()
+        
+   t=pulse_end-pulse_start
+   d=t*v/2
+
+
+   time.sleep(1)
+   # Toggle the output every second
+   print(d)
+ 
+   if d > 20 :
+    GPIO.output(output_pin, GPIO.LOW)
+  
+   else :
+        if d < 10 :
+            three = 0.5
+        else :
+            three=1
+        time.sleep(three)
+        GPIO.output(output_pin, GPIO.HIGH)
+        time.sleep(three)
+        GPIO.output(output_pin, GPIO.LOW)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    while True:
+        measure()
+
+GPIO.cleanup()
+
+
+
+
+
+
+
+
+
