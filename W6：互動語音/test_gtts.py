fbb99c5c59dab0a8d540ca18772a2d24872ec17d
楊逸平 <75960978+kuchyang@users.noreply.github.com>
2021-03-31 16:28:45 +0800

    Update test_gtts.py

--- W6：互動語音/test_gtts.py
+++ W6：互動語音/test_gtts.py
@@ -5,13 +5,13 @@ import numpy as np
 RMath = int(np.random.random()*100)
 if RMath>50:
     tts=gTTS(text='Continue Back~'+'You have'+str(RMath)+'centimeter', lang='en')
-    
 elif RMath > 30:
     tts=gTTS(text='Be careful。'+'You have'+str(RMath)+'centimeter', lang='en')    
 elif RMath > 10:
     tts=gTTS(text='Warning!'+'You have'+str(RMath)+'centimeter', lang='en')    
 else :
     tts=gTTS(text='Stop!'+'You have'+str(RMath)+'centimeter', lang='en')    
+
 print(RMath)
 
 tts.save("test_gtts.mp3")
